87910cf9072b86e032e48ae9912fef939de7f059
alissonfpmorais <alissonfpmorais@gmail.com>
2017-07-14 09:44:21 -0300

    Class 2
    Second class finished

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -1,6 +1,17 @@
 (ns forca.core
   (:gen-class))
 
+(def max-health 6)
+
+(defn end-game [] (print "You lose!"))
+
+(defn game [lifes]
+  (if (= lifes 0)
+    (end-game)
+    (do
+      (print lifes)
+      (game (dec lifes)))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
